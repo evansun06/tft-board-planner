e3c5b2fcd8e010964f3a2767610f417bf50b64a6
Evan Sun <evansun06@outlook.com>
2025-02-11 10:55:44 -0800

    "Finished Testing on ChampionInstance"

--- src/test/model/ChampionInstanceTest.java
+++ src/test/model/ChampionInstanceTest.java
@@ -7,11 +7,38 @@ import org.junit.jupiter.api.Test;
 public class ChampionInstanceTest {
     ChampionTemplate zeriTemplate;
     ChampionInstance zeri1;
+    ChampionInstance zeri2;
 
     @BeforeEach
     void setUp() {
-        zeriTemplate = new ChampionTemplate("Zeri", 100, 10, 5, 50, 10,10, 10, 6, 2);
+        zeriTemplate = new ChampionTemplate("Zeri", 100, 10, 5, 50, 10,10, 10, 10, 6, 2);
         zeri1 = new ChampionInstance(zeriTemplate, 1, 3);
+        zeri2 = new ChampionInstance(zeriTemplate, 3, 3);
+    }
+
+    @Test
+    void constructorTest() {
+        //Test superclass constructor
+        assertEquals("Zeri", zeri1.getName());
+        assertEquals("Zeri", zeri2.getName());
+        assertTrue(zeri1.readilyPlaceable);
+
+        //Test unique instance
+        assertFalse(zeri1.getInstanceId() == zeri2.getInstanceId());
+        assertEquals(1, zeri1.getInstanceId());
+        assertEquals(2, zeri2.getInstanceId());
+        //Test locations set
+        assertEquals(3, zeri2.getX());
+        assertEquals(3, zeri2.getY());
+
+        //Test static nextId
+        assertTrue(zeri1.getNextId() == zeri2.getNextId());
+        assertEquals(3, zeri2.getNextId());
+
+        //Test third unique instance
+        ChampionInstance zeri3 = new ChampionInstance(zeriTemplate, 4, 4);
+        assertEquals(3, zeri3.getInstanceId());
+
     }
 
 }
