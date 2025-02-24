f89792792a8cadf69aa1c363115a235812044d8e
Evan Sun <evansun06@outlook.com>
2025-02-24 12:19:58 -0800

    Specification of SetLoader Class

---
+++ src/main/persistance/SetLoader.java
@@ -0,0 +1,31 @@
+package persistance;
+
+import model.Set;
+
+// Set loader will read a constant JSON file with all the premade champions available.
+// This reduces startup runtime.
+public class SetLoader {
+    
+
+    // EFFECT: Assign set to the object and provides a relative file address 
+    // MODIFIES: this
+    public SetLoader() {
+
+    }
+
+    // EFFECT: Load Champions in the JSON to set
+    // MODIFIES: set
+    // REQUIRES: A properly formatted JSON file.
+    public void loadChampionsToSet(Set set) {
+
+    }
+
+    // GETTERS
+    public String getAddress() {
+        return "";
+    }
+
+    public Set getSet() {
+        return null;
+    }   
+}
