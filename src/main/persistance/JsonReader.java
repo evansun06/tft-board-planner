9f0aa791b8c2e2699859c5a0c6252aeac184e77a
Evan Sun <evansun06@outlook.com>
2025-02-25 18:19:00 -0800

    Finished implementation for loading championtemplates.

---
+++ src/main/persistance/JsonReader.java
@@ -0,0 +1,85 @@
+package persistance;
+
+import model.ChampionTemplate;
+import model.Set;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+import org.json.*;
+
+// JsonReader is multip functional object that can read json files.
+// Functionality includes loading set champions that are integral to the app,
+// also able to load previous board jsons.
+// This reduces startup runtime.
+public class JsonReader {
+    private String address;
+
+    // EFFECT: Assign set to the object and provides a relative file address 
+    // MODIFIES: this
+    public JsonReader(String address) {
+        this.address = address;
+    }
+
+    // EFFECT: Convert JSON file into a string with a given relative path.
+    //         thows IOException if not valid location.
+    public String readJson() throws IOException {
+        StringBuilder builder = new StringBuilder();
+
+        try (Stream<String> stream = Files.lines(Paths.get(this.address), StandardCharsets.UTF_8)) {
+            stream.forEach(string -> builder.append(string));
+        } 
+        return builder.toString();
+    }
+
+
+    // EFFECT: Load Champions in the JSON to set.
+    //       : Throws IOExcepiton if:
+    //          1. Address is invalid
+    //          2. Wrong JSON file
+    // MODIFIES: set
+    public void loadChampionsToSet(Set set) throws IOException {
+        setAddress("data/setThirteen.json");
+        String jsonData = readJson();
+        JSONObject setThirteenJson = new JSONObject(jsonData);
+        JSONArray championJsonArray = setThirteenJson.getJSONArray("champions");
+
+        for (Object championJson: championJsonArray) {
+            JSONObject champ = (JSONObject) championJson;
+            addChampion(set, champ);
+        }
+    }
+
+    // EFFECT: helper for loadChampionsToSet
+    //         loads a single ChampionTemplate to the Set
+    public void addChampion(Set set, JSONObject c) {
+        ChampionTemplate template = new ChampionTemplate(
+                c.getString("name"),
+                c.getInt("health"),
+                c.getInt("ar"),
+                c.getInt("mr"),
+                c.getInt("ad"),
+                c.getInt("ap"),
+                c.getInt("crit%"),
+                c.getInt("critX"),
+                c.getInt("atspd"),
+                c.getInt("range"),
+                c.getInt("cost")
+        );
+        set.templateAdd(template);
+    }
+
+
+    // EFFECT: changes address
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+    // GETTERS
+    public String getAddress() {
+        return this.address;
+    }
+  
+}
---
+++ src/test/persistance/JsonReaderTest.java
@@ -0,0 +1,69 @@
+package persistance;
+
+import static org.junit.Assert.*;
+import java.io.IOException;
+import javax.imageio.IIOException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import model.Set;
+
+// Used parts of JsonSerilizationDemo.JSONReaderTest UBC 210
+public class JsonReaderTest {
+    JsonReader testSetLoader;
+    Set testSet;
+
+    @BeforeEach
+    void setUp() {
+        testSetLoader = new JsonReader("data/setThirteen.json");
+        testSet = new Set("String");
+    }
+
+    @Test
+    void constructorTest() {
+        assertEquals("data/setThirteen.json", testSetLoader.getAddress());
+    }
+
+    @Test
+    void readJsonSetFaultyAddressTest() {
+        testSetLoader = new JsonReader("data/noSuchThing.json");
+        try {
+            testSetLoader.readJson();
+            fail("exception expected");
+        } catch (IOException e) {
+            // pass
+        }
+    }
+
+    @Test
+    void readJsonSetCorrectAddressTest() {
+        testSetLoader.setAddress("data/setThirteen.json");
+        String json = "";
+        try {
+             json = testSetLoader.readJson();
+        } catch (IOException e) {
+            fail("proper read expected");
+        }
+        assertFalse(json == null);
+    }
+
+    @Test
+    void loadChampionsToSetCorrectAddressTest() {
+        assertNull(testSet.findChampionTemplate("Ambessa"));
+
+        try {
+            testSetLoader.loadChampionsToSet(testSet);
+        } catch (IOException e) {
+            fail("Exception not expected");
+        }
+        assertEquals("Ambessa", testSet.findChampionTemplate("Ambessa").getName());
+        assertEquals(1100, testSet.findChampionTemplate("Ambessa").getHealth());
+    }
+
+
+    
+    
+
+    
+}
