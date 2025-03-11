d87ee1e6f926a3e10bc079292a0767a9e5122cfa
Evan Sun <evansun06@outlook.com>
2025-03-10 20:35:38 -0700

    Implemented MouseListener functions for yes/no buttons in the session recovery prompt

--- src/main/ui/MainMenuGUI.java
+++ src/main/ui/MainMenuGUI.java
@@ -3,17 +3,18 @@ package ui;
 import javax.swing.*;
 
 import persistance.JsonReader;
-import persistance.JsonWriter;
 import model.*;
 
-import java.awt.BorderLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+
 
 // MainMenuGUI is the graphical interface that users are first introduced to
 //
-public class MainMenuGUI extends JFrame {
-    private Planner planner;
+public class MainMenuGUI extends JFrame { 
+    protected Planner planner;
     private JsonReader jsonReader = new JsonReader("data/userPersistance.json");
-    private JsonWriter jsonWriter = new JsonWriter("data/userPersistance.json");
     private JDesktopPane desktopPane;
 
 
@@ -41,26 +42,68 @@ public class MainMenuGUI extends JFrame {
         JInternalFrame popup = new JInternalFrame("Session Recovery", true, false, false, false);
         popup.setSize(500,200);
         popup.setLayout(null);
+
         desktopPane.add(popup);
+        
         popup.setResizable(false);
         JLabel prompt = new JLabel("Do you want to recover your previous session?");
-        prompt.setBounds(75, 50,400,30);
+        prompt.setBounds(100, 50,300,30);
         popup.add(prompt);
 
-        JButton yes = new JButton("Yes");
-        JButton no = new JButton("No");
-        yes.setBounds(120, 100, 100, 30);
-        no.setBounds(230, 100, 100, 30);
-        popup.add(yes);
-        popup.add(no);
-
+        addSessionRecoveryButtons(popup);
         //Dynamically Center
         popup.setLocation((this.getWidth() / 2 - popup.getWidth() / 2),
                 (this.getHeight() / 2 - popup.getHeight() / 2));
         popup.setVisible(true);
-        
+    }
+    
+    //REQUIRES: The exact JInternalFrame used for session recovery needs to be passed
+    //EFFECTS: Creates and display buttons while handling their responses
+    public void addSessionRecoveryButtons(JInternalFrame recoveryPopup) {
+        JButton yes = new JButton("Yes");
+        JButton no = new JButton("No");
+        yes.setBounds(140, 100, 100, 30);
+        no.setBounds(260, 100, 100, 30);
+        recoveryPopup.add(yes);
+        recoveryPopup.add(no);
+        handleSessionRecoveryButtons(yes, no, recoveryPopup);
+       
     }
 
+    // EFFECT: Adds mouseListener functions for the Yes/No MouseListener to the RecoveryPopup JInternalFrame
+    public void handleSessionRecoveryButtons(JButton yes, JButton no, JInternalFrame recoveryPopup) {
+        yes.addMouseListener(new MouseAdapter() {
+            @Override
+            // EFFECT: reads JSON to this.planner state and disposes of the prompt popup
+            public void mouseClicked(MouseEvent e) {
+                if (e.getButton() == MouseEvent.BUTTON1) {
+                    loadJson();
+                    recoveryPopup.dispose();
+                }
+            }
+        });   
+
+        no.addMouseListener(new MouseAdapter() {
+            @Override
+            // EFFECT: creates a new Planner with no boards and disposes of the prompt popup
+            public void mouseClicked(MouseEvent e) {
+                if (e.getButton() == MouseEvent.BUTTON1) {
+                    planner = new Planner();
+                    recoveryPopup.dispose();
+                }
+            }
+        });
+    }
+
+    //EFFECT: Load JSON to this.planner 
+    //        If IOException print stack trace.
+    public void loadJson() {
+        try {
+            this.planner = jsonReader.plannerJsonToObject();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 
     
